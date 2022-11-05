d005fbfc765715069587f6921a0f566f45433fa1
URK21CS3008DINESHKANNAN <103260244+URK21CS3008DINESHKANNAN@users.noreply.github.com>
2022-11-05 22:21:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 class BinaryTreeNode:
     def __init__(self, data):
         self.data = data
-        self.left_child = None
-        self.right_child = None
+        self.left = None
+        self.right = None
 
 
 def insert(root, new_value) -> BinaryTreeNode:
@@ -11,43 +11,53 @@ def insert(root, new_value) -> BinaryTreeNode:
         If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
         Finally, return the root.
         """
-    if not root:
+    # Write your code here
+    if root == None:
         root = BinaryTreeNode(new_value)
-        return root
-    if new_value < root.data:
-         if root.left_child:
-            insert(root.left_child, new_value)
-         else:
-            root.left_child = BinaryTreeNode(new_value)
+    elif new_value < root.data:
+        root.left = insert(root.left,new_value)
     else:
-        if root.right_child:
-            insert(root.right_child, new_value)
-        else:
-            root.right_child = BinaryTreeNode(new_value)
+        root.right = insert(root.right,new_value)
+    return root
 
 
 def inorder(root) -> None:
     # Write your code here
-    if root is None:
-        return
-    inorder(root.left_child)
-    print(root.data, end = " ")
-    inorder(root.right_child)
+    if root:
+        inorder(root.left)
+        print(root.data, end =' ')
+        inorder(root.right)
 
 
 def preorder(root) -> None:
     # Write your code here
-    if root is None:
-        return
-    print(root.data, end = " ")
-    preorder(root.left_child)
-    preorder(root.right_child)
+    if root:
+        print(root.data, end=' ')
+        preorder(root.left)
+        preorder(root.right)
 
 
 def postorder(root) -> None:
     # Write your code here
-    if root is None:
-        return
-    postorder(root.left_child)
-    postorder(root.right_child)
-    print(root.data, end = " ")
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data, end = ' ')
+
+# Do not change the following code
+input_data = input()
+flag = True
+root = None
+for item in input_data.split(', '):
+    if flag is True:
+        root = insert(None, int(item))
+        flag = False
+    else:
+        insert(root, int(item))
+inorder(root)
+print()
+preorder(root)
+print()
+postorder(root)
+Footer
+© 2022 GitHub, Inc.
